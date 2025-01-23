e3d4a3269167c94f355df96d3d02133500876ff7
Wadje <toddnunes0@gmail.com>
2025-01-23 11:39:38 -0300

    Add ENDPOINT Wadje

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -36,10 +36,5 @@ public class HomeController {
   public @ResponseBody String greetingRafael() { 
     return "Hello, Wadje!"; 
   }
-  
-  @RequestMapping("/Wadje_branch") 
-  public @ResponseBody String greetingRafaelBranch() { 
-    return "Hello, Wadje! (branch)"; 
-  }
-  
+ 
 }  
